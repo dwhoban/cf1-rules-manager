35a596597b4d778f0d2f5df4a47b8862aa5e3ddc
Dan Hoban <5687870+dwhoban@users.noreply.github.com>
2026-02-24 10:05:35 +1000

    feat(tools): Setup drizzle-kit for schema managment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import { config } from 'dotenv'
-import { defineConfig } from 'drizzle-kit'
-
-config({ path: ['.env.local', '.env'] })
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'sqlite',
+  out: "./migrations",
+  schema: "./src/db/schema.ts",
+  dialect: "sqlite",
+  driver: "d1-http",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+    token: process.env.CLOUDFLARE_API_TOKEN!,
   },
-})
+});
